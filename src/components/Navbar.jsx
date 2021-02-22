38e11b3c50a405485b436f0205265b1f22ec0ccd
Michael Grebowicz <maciejgrebowicz@gmail.com>
2021-02-22 14:22:31 -0800

    finish Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
 
 export default function Navbar() {
     const [click, setClick] = useState(false);
-    
-    const handleClick = () => setClick(!click);
+    const [button, setButton] = useState(true);
 
+    const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+    
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton()
+    }, [])
+
+    window.addEventListener('resize', showButton);
+    
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
                         TRVL <i className="fab fa-typo3"></i>
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
@@ -38,7 +55,8 @@ export default function Navbar() {
                                 Sign Up
                             </Link>
                         </li>    
-                    </ul> 
+                    </ul>
+                    {button && <Button buttonStyle="btn--outline">SIGN UP</Button>} 
                 </div>
             </nav>
         </>
